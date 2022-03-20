da26b4f72cf9757bd472691b31593b71ad921506
D_037 <63572694+LeonardBrowntail@users.noreply.github.com>
2022-03-20 21:46:27 +0700

    Add project files.

---
+++ Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace iSpyMatchmaker
+{
+    internal class Client
+    {
+        public static int dataBufferSize = 4096;
+
+        public int id;
+        public TCP tcp;
+
+        public Client(int _clientId)
+        {
+            id = _clientId;
+            tcp = new(_clientId);
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+            private NetworkStream stream;
+            private byte[] recieveBuffer;
+
+            public TCP(int _id)
+            {
+                id = _id;
+            }
+
+            public void Connect(TcpClient _socket)
+            {
+                socket = _socket;
+                socket.ReceiveBufferSize = dataBufferSize;
+                socket.SendBufferSize = dataBufferSize;
+
+                stream = socket.GetStream();
+
+                recieveBuffer = new byte[dataBufferSize];
+
+                stream.BeginRead(recieveBuffer, 0, dataBufferSize, RecieveCallback, null);
+            }
+
+            private void RecieveCallback(IAsyncResult _result)
+            {
+                try
+                {
+                    int _bytelength = stream.EndRead(_result);
+                    if (_bytelength <= 0)
+                    {
+                        return;
+                    }
+
+                    byte[] _data = new byte[_bytelength];
+                    Array.Copy(recieveBuffer, _data, _bytelength);
+
+                    stream.BeginRead(recieveBuffer, 0, dataBufferSize, RecieveCallback, null);
+                }
+                catch (Exception _ex)
+                {
+                    Console.WriteLine($"Error recieving data: {_ex}");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace iSpyMatchmaker
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "iSpy Matchmaker";
+            var server = new Server();
+            server.Start(50, 8888);
+
+            Console.ReadKey();
+        }
+    }
+}
